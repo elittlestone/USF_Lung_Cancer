4b16a72f2ec6dec432b4db55755f8c8642c1d128
Ethan Littlestone <ethanlittlestone@gmail.com>
2025-07-08 14:57:42 -0400

    updated

--- compare_txrn1_expressions.py
+++ compare_txrn1_expressions.py
@@ -5,8 +5,11 @@ from scipy.io import mmread
 import anndata as ad
 import scanpy as sc
 import os 
+import matplotlib as mtplt
+mtplt.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.stats import mannwhitneyu
+from tqdm import tqdm
 
 def main():
 
@@ -14,6 +17,8 @@ def main():
     output_directory = "results/figures"
     os.makedirs(output_directory, exist_ok=True)
     sc.settings.figdir = output_directory
+    sc.settings.verbosity = 'debug'
+
 
     parser = argparse.ArgumentParser(description = "Txrn1 expression comparison script")
 
@@ -32,9 +37,13 @@ def main():
     args = parser.parse_args()
     matrix_file, genes, barcodes, annotation_file = args.expression_matrix_file, args.genes, args.barcodes, args.annotation_file
 
-    read_exp_matrix(matrix_file, genes, barcodes, annotation_file)
+    # Read in expression matrix and genes, barcodes
+    adata = read_exp_matrix(matrix_file, genes, barcodes)
+    adata = plots_and_mannwhitney(adata, annotation_file)
+    post_processing_and_clustering(adata)
 
-def read_exp_matrix(matrix_file, genes, barcodes, annotation_file):
+
+def read_exp_matrix(matrix_file, genes, barcodes):
     # Load matrix
     matrix = mmread(matrix_file).tocsr().T
 
@@ -48,6 +57,17 @@ def read_exp_matrix(matrix_file, genes, barcodes, annotation_file):
     adata.var_names = genes
     adata.obs_names = barcodes
 
+    # Normalize expression
+    sc.pp.normalize_total(adata)
+    sc.pp.log1p(adata)
+
+    # Scale 
+    sc.pp.scale(adata)
+
+    return adata
+
+def plots_and_mannwhitney(adata, annotation_file):
+
     # Load cell-type annotations
     annotations_dict = pyreadr.read_r(annotation_file)
     annotations_df = annotations_dict[None]
@@ -56,46 +76,83 @@ def read_exp_matrix(matrix_file, genes, barcodes, annotation_file):
     # Rename the annotation column to "cell_type"
     annotations_df = annotations_df.rename(columns={"truth": "cell_type"})
 
+    # Get counts for each cell type
+
+    celltype_counts = annotations_df["cell_type"].value_counts()
+    print(f"Cell type counts: {celltype_counts}")
+
     # Merge the annotations table with the anndata object 
     adata.obs = adata.obs.join(annotations_df, how="left")
 
     # Plot to compare expression across cell-types (cancer cells vs immune cells)
-    sc.pl.violin(adata, keys="TXNRD1", groupby="cell_type", show = False, stripplot = True)
-    plt.gcf().set_size_inches(12, 6)
-    plt.xticks(rotation = 45, ha = 'right')  # rotate labels for better readability
-    plt.savefig("results/figures/violin_cancer_vs_immune_cells.png")
-    #plt.show()
+    genes_of_interest = ["TXNRD1", "TXN"]
 
-    sc.pl.dotplot(adata, "TXNRD1", groupby="cell_type", save = "_cancer_vs_immune.png")
+    # Initalize results list to then output to CSV 
+    results = []
+    reference_group = "Cancer cells"
+
+    #matches = [var for var in adata.var_names if "txn1" in var.lower()]
+    for gene in tqdm(genes_of_interest):
+        # First create summary plots, then perform statistical analyses
+
+        # Violin plot
+        sc.pl.violin(adata, keys=gene, groupby="cell_type", show = False, stripplot = True)
+        plt.gcf().set_size_inches(12, 6)
+        plt.xticks(rotation = 45, ha = 'right')  # rotate labels for better readability
+        plt.savefig(f"results/figures/violin_{gene}_cancer_vs_immune_cells.png")
+        plt.close("all")
+
+        # Dotplot
+        sc.pl.dotplot(adata, gene, groupby="cell_type", show = False)
+        plt.savefig(f"results/figures/dotplot_{gene}_cancer_vs_immune_cells.png")
+        plt.close("all")
+
+        # Statistical tests 
+        cancer_expr = adata[adata.obs["cell_type"] == "Cancer cells", gene].X
+        cancer_expr = cancer_expr.toarray().flatten()
+        cancer_mean = cancer_expr.mean()
+
+        # Perform Mann-Whitney U for each cell type against cancer cells
+        for cell_type in adata.obs["cell_type"].unique():
+            if cell_type == reference_group:
+                expression = cancer_expr
+                mean_expression = cancer_mean
+                stat, pval = None, None
+            else:
+                expression = adata[adata.obs["cell_type"] == f"{cell_type}", gene].X
+                expression = expression.toarray().flatten()
+                mean_expression = expression.mean()
+                stat, pval = mannwhitneyu(cancer_expr, expression, alternative = "two-sided")
+   
+            
+            # Store results for each cell-type and each gene in a dictionary
+            results.append({
+                "Gene": gene,
+                "Cell Type": cell_type,
+                "Mean Expression": mean_expression,
+                "P-value": pval,
+            })
+
+    # Output results/statistics to CSV 
+    results_df = pd.DataFrame(results)
+    results_df.sort_values(by = ["Cell Type", "Gene"], inplace = True)
+    results_df.to_csv("results/mannwhitney_celltype_comparison.csv", index = False)
+
+    return adata
 
 
-    cancer_expr = adata[adata.obs["cell_type"] == "Cancer cells", "TXNRD1"].X
-    immune_expr = adata[adata.obs["cell_type"] != "", "TXNRD1"].X
+def post_processing_and_clustering(adata):
 
-    cancer_expr = cancer_expr.toarray().flatten() if hasattr(cancer_expr, "toarray") else cancer_expr.flatten()
-    immune_expr = immune_expr.toarray().flatten() if hasattr(immune_expr, "toarray") else immune_expr.flatten()
+    # Perform PCA, kNN, UMAP
+    sc.tl.pca(adata)
+    sc.pp.neighbors(adata)
+    sc.tl.umap(adata)
 
-    stat, pval = mannwhitneyu(cancer_expr, immune_expr, alternative="two-sided")
-    print(f"Wilcoxon test p-value: {pval}")
-
-
-    # Perform PCA, NN, UMAP
-    #sc.tl.pca(adata)
-    #sc.pp.neighbors(adata)
-    #sc.tl.umap(adata)
-    #sc.pl.umap(adata, color="TXNRD1", save="_TXNRD1_expression.png")
-
-
-
-    
-
-    #mean_exp = adata.obs.groupby("cell_type")["TXNRD1"].mean()
-    #mean_exp.plot(kind='bar')
-    #plt.ylabel("Mean TXNRD1 expression")
-    #plt.savefig("mean_exp_barplot.png")
-    #plt.show()
-
-
+    # Plot UMAP
+    genes_of_interest = ["TXNRD1", "TXN"]
+    for gene in tqdm(genes_of_interest):
+        sc.pl.umap(adata, color="cell_type", show=False, save = f"_{gene}_celltypes.pdf")
+        sc.pl.umap(adata, color = gene, show=False, cmap = "inferno", vmin = 0, size = 8, vmax = "p99.5", save = f"_{gene}_expression.pdf")
 
 if __name__ == "__main__":
     main()
